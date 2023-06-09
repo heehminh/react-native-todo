317e1ff5170943ddc2fb9feb7d94d2c94121daae
heehminh <mink141416@gmail.com>
2023-06-09 21:27:16 +0900

    Feat: MainScreen Style

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 
@@ -20,4 +20,31 @@ const MainScreen = () => {
 
 export default MainScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? 20 : 0,
+    backgroundColor: "#f7f8fa",
+  },
+  pageTitle: {
+    marginBottom: 35,
+    fontSize: 54,
+    fontWeight: "600",
+  },
+  listView: {
+    flex: 1,
+  },
+  listTitle: {
+    marginBottom: 25,
+    paddingHorizontal: 15,
+    fontSize: 41,
+    fontWeight: "500",
+  },
+  separator: {
+    marginHorizontal: 10, // marginLeft, marginRight 동시에
+    marginTop: 25,
+    marginBottom: 10,
+    borderBottomColor: "rgba(0,0,0,0.2",
+    borderBottomWidth: 1,
+  },
+});
