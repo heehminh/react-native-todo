7771df54efc1d4950bcbff9e7119c6905e588f0b
heehminh <mink141416@gmail.com>
2023-06-09 22:11:09 +0900

    Feat: 할일입력 Style

--- components/InputForm.js
+++ components/InputForm.js
@@ -13,10 +13,11 @@ const InputForm = () => {
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.addFormContainer}
     >
-      <TextInput placeholder="할 일을 적어주세요." />
-      <Pressable>
-        <Text>+</Text>
+      <TextInput style={styles.inputField} placeholder="할 일을 적어주세요." />
+      <Pressable style={styles.addButton}>
+        <Text style={styles.addButtonText}>+</Text>
       </Pressable>
     </KeyboardAvoidingView>
   );
@@ -24,4 +25,43 @@ const InputForm = () => {
 
 export default InputForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  addFormContainer: {
+    flexDirection: "row",
+    marginTop: "auto",
+    marginBottom: 30,
+    marginHorizontal: 20,
+    backgroundColor: "#f7f8fa",
+  },
+  inputField: {
+    flex: 1,
+    height: 42,
+    padding: 5,
+    marginRight: 25,
+    borderRadius: 4,
+    borderColor: "rgba(0, 0, 0, 0.2)",
+    borderWidth: 1,
+    fontSize: 16,
+    color: "#000000",
+    textAlignVertical: "center",
+  },
+  addButton: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 42,
+    hegiht: 42,
+    borderRadius: 4,
+    backgroundColor: "rgba(0,0,0,0.7)",
+    shadowColor: "#000",
+    shadowRadius: 8,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+  },
+  addButtonText: {
+    fontSize: 25,
+    color: "#fff",
+    textAlign: "center",
+  },
+});
